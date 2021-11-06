425a69764bf9ed28c540233bca9647ea2b2dd74c
QFrankQ <frank2001313@gmail.com>
2021-11-06 14:39:26 -0400

    chrome driver setup
    chrome driver set up code for quizlet bot. chrome driver in project directory is for windows, chrome version 95.0

--- .ipynb_checkpoints/QuizletBot-checkpoint.py
+++ .ipynb_checkpoints/QuizletBot-checkpoint.py
@@ -4,8 +4,9 @@ use selenium
 
 '''
 
-
+from selenium import webdriver
 
 
 class QuizletBot():
-    
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path = './chromedriver')
